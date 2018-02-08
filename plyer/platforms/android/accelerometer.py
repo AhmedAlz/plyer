de2657190769f22b8f2406a33245d59efc590531
AhmedAlz <a8888h@gmail.com>
2018-02-07 17:45:30 -0800

    Update accelerometer.py
    CHANGED TO FASTEST

--- plyer/platforms/android/accelerometer.py
+++ plyer/platforms/android/accelerometer.py
@@ -30,7 +30,7 @@ class AccelerometerSensorListener(PythonJavaClass):
     def enable(self):
         self.SensorManager.registerListener(
             self, self.sensor,
-            SensorManager.SENSOR_DELAY_GAME
+            SensorManager.SENSOR_DELAY_FASTEST
         )
 
     def disable(self):
